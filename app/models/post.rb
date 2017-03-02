358d4cc1e333a1b3904165d70176a59731032b20
fakebenjay <fakebenjay@gmail.com[color]>
2017-03-02 18:06:25 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category, you insipid fuckhat." }
+  validate :clickbait?
+
+  def clickbait?
+    clickbait_terms = ["Won't Believe", "Secret", "Guess"]
+    if self.title && !clickbait_terms.any? { |c| self.title.include?(c)}
+      errors.add(:title, "No YOU'RE fake news!!!")
+    end
+  end
 end
